eb057dfb025ab46c2ee105edd884e1112f9176ef
JosephRobson <joseph.robson@hotmail.co.uk>
2021-02-12 12:07:10 +0000

    lesson 1 completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import StarWarsCharacters from "./components/StarWarsCharacters";
 
-  function App() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-      </div>
-    );
-  }
-  
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <StarWarsCharacters />
+    </div>
+  );
+}
+
 export default App;
